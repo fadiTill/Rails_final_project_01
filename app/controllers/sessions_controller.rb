c5103671f03466637dbe16feb96dcc5d74ca4a87
github username <github email address>
2020-06-30 13:08:43 -0700

    env

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
+
+  def create
+    pp request.env['omniauth.auth']
+    session[:name] = request.env['omniauth.auth']['info']['name']
+    session[:omniauth_data] = request.env['omniauth.auth']
+
    
+    redirect_to root_path
   end
+end
+   
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- 
-
-
-
-end
+  resources :welcome, only: [:home] do
+  end
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  # Add your routes here		   # Add your routes here
+  root 'welcome#home'
+end		 
