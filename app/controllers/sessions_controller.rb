5f746b4ebb234ba07eb2bd2c7cc80b5119af846f
github username <github email address>
2020-07-01 15:06:06 -0700

    sessions, view, controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,26 @@ class SessionsController < ApplicationController
    
   #   redirect_to root_path
   # end
+
+  def new 
+
+  end
+
+
+  def create 
+    @user = User.find_by(email: params[:user][:email])
+    session[:user_id] = @user.id
+    redirect user_path
+  else
+    redirect_to '/'
+
+  end
+
+def delete
+  session.destroy
+  redirect_to root_path
+  end 
+
 end
    
   
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   end
   # match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   # # Add your routes here		   # Add your routes here
+
+
+
+   get '/login', to: 'sessions#new'
+   post "/login", to: "sessions#create"
+   delete "/logout", to: "sessions#destroy" 
    root 'welcome#home'
  end		 
