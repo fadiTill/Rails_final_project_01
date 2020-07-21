3fd943b82016e5e8e0979adf82de950912915b04
github username <github email address>
2020-07-20 20:25:39 -0700

    omniauth login path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,44 +1,54 @@
 require 'pry'
 
 class SessionsController < ApplicationController
-  #  skip_before_action :verify_authenticity_token, only: :create
-
-  # def create
-  #   pp request.env['omniauth.auth']
-  #   session[:name] = request.env['omniauth.auth']['info']['name']
-  #   user = User.sign_in_from_aumniauth(auth)
-  #   session[:user_id] = @user.id
-  #   session[:omniauth_data] = request.env['omniauth.auth']
-
-   
-  #   redirect_to root_url, notice: "SIGNED IN"
-  # end
-
-
-
-#   def new 
-
-#   end
-
-
-  def create 
-    @user = User.find_by(email: params[:email])
-   if @user && @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-    redirect_to user_path(@user)
-   else 
-     render 'new'
-    
-  end
-end
-
   
 
-    def destroy
-      session.destroy
+  def new 
+  end
+
+
+  def create
+    if auth_hash = request.env["omniauth.auth"]
+      @user = User.find_or_create_by_omniauth(auth_hash)
+      session[:user_id] = @user.id
+      redirect_to signin_path
+    else
+      @user = User.find_by(email: params[:email])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        render 'new'
+      end
+    end
+  end
+  
+
+  def destroy
+      reset_session
       redirect_to root_path
       end 
     end
+
+
+#   def create 
+#     @user = User.find_by(email: params[:email])
+#    if @user && @user.authenticate(params[:password])
+#     session[:user_id] = @user.id
+#      redirect_to user_path(@user)
+#    else 
+#       render 'new'
+    
+#   end
+# end
+
+  
+
+    # def destroy
+    #   session.destroy
+    #   redirect_to root_path
+    #   end 
+    # end
 
 
 # def create
