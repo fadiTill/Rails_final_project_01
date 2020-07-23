6aa1a315596383f4caee8f01ee9219f71e7a042d
github username <github email address>
2020-07-23 14:04:03 -0700

    homework scope test

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -8,4 +8,11 @@ class  Guest < ApplicationRecord
      validates :name, presence: true
      validates :email, presence: true, uniqueness: true
      validates :phone_number, presence: true, uniqueness: true
+
+#homework scope
+     scope :test, -> { where(comment: 'test') } do
+
+          
+         
+end
 end
